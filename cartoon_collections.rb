503efe132a1dcef7979b21804e0048022cb34a29
Joshua Hill <josh@jlhworkshop.com>
2019-07-31 22:06:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,6 +23,8 @@ end
 def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  if array.include?(cheese_types) 
+  if array.include?(cheese_types) {
+    return array
+  }
     
 end
