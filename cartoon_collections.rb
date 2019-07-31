42510f1ff69e5fbf5add64cff5afa35e34bb6b2e
Joshua Hill <josh@jlhworkshop.com>
2019-07-31 21:58:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ end
 #summon_captain_planet(planeteer_calls)
 
 def long_planeteer_calls(array)
-  array.any? { |word| if word.length > 4}
+  array.any? { |word| word.length > 4}
 end
 
 def find_the_cheese# code an argument here
