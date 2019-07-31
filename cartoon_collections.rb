943079edfc946714f6da78804df8cd0008a76fa1
Joshua Hill <josh@jlhworkshop.com>
2019-07-31 21:37:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,15 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index { |item, index|
+     array[index] = "#{index+1}." + item
+  }
+  puts array
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+# roll_call_dwarves(["Doc", "Dopey", "Bashful", "Grumpy"])
+
+def summon_captain_planet(array)
+  array.map { |name| name.capitalize
+  
 end
 
 def long_planeteer_calls# code an argument here
